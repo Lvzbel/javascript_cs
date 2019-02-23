e93ac058ca2ff99e4f2cde04293cc7b072745a79
Rodrigo <rcoto@protonmail.com>
2019-02-22 21:14:07 -0800

    Power Solution Completed It

--- Recursion/power.js
+++ Recursion/power.js
@@ -1,16 +1,20 @@
 function power(base, exponent) {
-  let result;
+  let result = base;
+  let counter = exponent;
 
-  function helper(base, exponent) {
-    if (exponent <= 0) return result;
-    result += base + base;
-    helper(base, exponent - 1);
+  function helperPower(base1) {
+    if (counter === 0) result = 1;
+    if (counter <= 1) return;
+    result *= base;
+    counter -= 1;
+    helperPower(base1);
   }
 
-  helper(base, exponent);
+  helperPower(base);
   return result;
 }
 
 console.log(power(2, 0)); // 1
 console.log(power(2, 2)); // 4
 console.log(power(2, 4)); // 16
+console.log(power(5, 4)); // 16
